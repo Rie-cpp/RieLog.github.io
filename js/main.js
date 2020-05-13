0535d6870375b7b378107a6dac5311f26d87f257
Rie-cpp <hgrt0822@gmail.com>
2020-05-13 22:58:22 +0900

    add loading menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+  let loader = document.getElementById('my-loader');
+
+  // ..loader-wrap に .loaded を追加してローディング表示を消す
+  loader.classList.add('loaded');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById("menuButton").addEventListener("click", function() {
 		this.classList.toggle("active");
